aa95b513892b739ecca8c26506ade04703197c55
amazingawwal <sakeebmawwal@gmail.com>
2025-08-21 18:33:25 +0100

    feat:reading and writing to a file

---
+++ task4.py
@@ -0,0 +1,19 @@
+# # Program that reads a file
+
+# with open('greetings.txt', 'r') as greet:
+#     greeting = greet.read()
+#     print(greeting)
+
+# # writes a modified version to a new file.
+# with open('response.txt', 'w') as response:
+#     response.write(f"{greeting}, How are you all doing today?")
+
+
+file_name = input("Enter your file name: ")
+
+try:
+    if file_name:
+        file = open(file_name, 'r')
+        print(file.read())
+except:
+    print("No file name provided")
