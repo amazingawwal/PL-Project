50dee56a72fd41a579dc194432f272f9a4151266
amazingawwal <sakeebmawwal@gmail.com>
2025-08-21 18:51:37 +0100

    feat:error handling

--- task4.py
+++ task4.py
@@ -1,19 +1,21 @@
-# # Program that reads a file
+# Program that reads a file
 
-# with open('greetings.txt', 'r') as greet:
-#     greeting = greet.read()
-#     print(greeting)
+with open('greetings.txt', 'r') as greet:
+    greeting = greet.read()
+    print(greeting)
 
-# # writes a modified version to a new file.
-# with open('response.txt', 'w') as response:
-#     response.write(f"{greeting}, How are you all doing today?")
+# writes a modified version to a new file.
+with open('response.txt', 'w') as response:
+    response.write(f"{greeting}, How are you all doing today?")
 
-
+# Error Handling
 file_name = input("Enter your file name: ")
 
 try:
     if file_name:
         file = open(file_name, 'r')
         print(file.read())
+    else:
+        raise Exception
 except:
     print("No file name provided")
